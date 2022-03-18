cac260175b8733aad56a6de97435a95310f197f7
Moustafa M. Elgammal <mostafa.elgammal@aboughalymotors.com>
2022-03-18 17:41:28 +0200

    services and schools services

---
+++ app/Services/Schools/SchoolsServices.php
@@ -0,0 +1,77 @@
+<?php
+namespace App\Services\Schools;
+use App\Factories\SchoolFactoryDB;
+use App\Models\School;
+use App\Repositories\Schools\SchoolsRepository;
+use App\Services\Services;
+use Illuminate\Http\Request;
+
+class SchoolsServices extends Services
+{
+    private SchoolsRepository $schoolRepository;
+    private School $SchoolModel;
+    private $schoolFactory;
+
+    public function __construct()
+    {
+        $this->schoolRepository = new SchoolsRepository();
+        $this->schoolFactory = new SchoolFactoryDB();
+        $this->SchoolModel = new School();
+    }
+
+    public function createSchool(Request $request)
+    {
+        $data = new \stdClass();
+        $data->name = $request->name;
+
+        if(!$this->schoolRepository->create($this->schoolFactory, $data))
+        {
+            foreach ($this->schoolRepository->getErrors() as $error)
+                $this->addError($error);
+
+            return false;
+        }
+
+        return true;
+    }
+
+    public function updateSchool(Request $request)
+    {
+        $data = new \stdClass();
+        $data->id = $request->id;
+        $data->name = $request->name;
+
+        if(!$this->schoolRepository->update($this->schoolFactory, $data))
+        {
+            foreach ($this->schoolRepository->getErrors() as $error)
+                $this->addError($error);
+
+            return false;
+        }
+
+        return true;
+    }
+
+    public function deleteSchool(Request $request)
+    {
+        if(!$this->schoolRepository->delete($this->SchoolModel, $request->id))
+        {
+            foreach ($this->schoolRepository->getErrors() as $error)
+                $this->addError($error);
+
+            return false;
+        }
+
+        return  true;
+    }
+
+    public function getAllSchools()
+    {
+        return $this->schoolRepository->getAll($this->SchoolModel);
+    }
+
+    public function getSchoolById(Request $request)
+    {
+        return $this->schoolRepository->getById($this->SchoolModel, $request->id);
+    }
+}
---
+++ app/Services/Services.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Services;
+
+use App\Errors\Errors;
+
+class Services extends Errors
+{
+
+}
