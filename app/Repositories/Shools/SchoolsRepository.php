3f09e1b3c0392520d9ba95f1af0683884990795c
Moustafa M. Elgammal <mostafa.elgammal@aboughalymotors.com>
2022-03-18 17:38:36 +0200

    schools Repository

---
+++ app/Repositories/Shools/SchoolsRepository.php
@@ -0,0 +1,12 @@
+<?php
+namespace  App\Repositories\Schools;
+
+use App\Repositories\Repository;
+use App\Repositories\RepositoryInterface;
+use Illuminate\Database\Eloquent\Model;
+use phpDocumentor\Reflection\Types\Collection;
+
+class SchoolsRepository extends Repository
+{
+
+}
