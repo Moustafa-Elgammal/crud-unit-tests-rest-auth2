6b84d285825305be0bcdb1fa3856545f337bae01
Moustafa M. Elgammal <mostafa.elgammal@aboughalymotors.com>
2022-03-18 13:43:59 +0200

    schools seeder

---
+++ database/seeders/SchoolSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\School;
+use Illuminate\Database\Seeder;
+
+class SchoolSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        School::factory()->count(2)->create();
+    }
+}
